49457f1206d07900dceb5d35f96012713841019f
Billy Gigurtsis <bgigurtsis@gmail.com>
2026-03-01 21:35:07 +0000

    Fix install: use python3 -m venv instead of virtualenv_create
    virtualenv_create tried to run "python@3.12" as a literal binary
    (doesn't exist) and created venvs --without-pip. Switch to plain
    python3 -m venv which includes pip, then pip install fetches all
    PyPI dependencies. Also fix lowercase entry point symlink.

--- Formula/klaus.rb
+++ Formula/klaus.rb
@@ -1,10 +1,10 @@
 # Homebrew formula for Klaus
 #
-# To use this formula, create a tap repo (homebrew-Klaus) containing this file
-# at Formula/Klaus.rb, then:
+# To use this formula, create a tap repo (homebrew-klaus) containing this file
+# at Formula/klaus.rb, then:
 #
-#   brew tap <your-github-user>/Klaus
-#   brew install Klaus
+#   brew tap <your-github-user>/klaus
+#   brew install klaus
 #
 # Before publishing, update the `url` to point to a tagged release tarball and
 # regenerate the sha256 with:
@@ -12,8 +12,6 @@
 #   shasum -a 256 Klaus-0.1.0.tar.gz
 
 class Klaus < Formula
-  include Language::Python::Virtualenv
-
   desc "Voice-powered research assistant for physical books and papers"
   homepage "https://github.com/bgigurtsis/Klaus"
   url "https://github.com/bgigurtsis/Klaus/archive/refs/tags/v0.1.0.tar.gz"
@@ -24,9 +22,10 @@ class Klaus < Formula
   depends_on "portaudio"
 
   def install
-    virtualenv_create(libexec, "python@3.12")
-    system libexec/"bin/pip3", "install", "--no-cache-dir", buildpath
-    bin.install_symlink libexec/"bin/Klaus"
+    python3 = Formula["python@3.12"].opt_bin/"python3.12"
+    system python3, "-m", "venv", libexec
+    system libexec/"bin/pip", "install", buildpath
+    bin.install_symlink libexec/"bin/klaus"
   end
 
   def caveats
@@ -37,11 +36,11 @@ class Klaus < Formula
       On first launch, a setup wizard will walk you through API key
       configuration, camera selection, and voice model download.
 
-      Run `Klaus` to start.
+      Run `klaus` to start.
     EOS
   end
 
   test do
-    assert_match "usage", shell_output("#{bin}/Klaus --help 2>&1", 2)
+    assert_match "usage", shell_output("#{bin}/klaus --help 2>&1", 2)
   end
 end
